c03b9975c0d9aa501f2847ddb59bdd9269868d63
minisola <blackkid1989@gmail.com>
2019-06-27 22:44:58 +0800

    basic function

--- extension.js
+++ extension.js
@@ -1,36 +1,62 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const request = require('request');
+const exec = require('child_process').exec;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	const hitokoto = {
+
+	}
+
 	console.log('Congratulations, your extension "hitokoto" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	const disposable = vscode.commands.registerCommand('extension.hitokoto', function () {
+		createHitokoto(hitokoto)
+	});
+	const getText = vscode.commands.registerCommand('extension.openHitokoto', function () {
+		// vscode.workspace.openTextDocument('http://baidu.com')
+	exec(`start https://hitokoto.cn?id=${hitokoto.id}`)
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(getText);
+	//start
+	createHitokoto(hitokoto)
 }
+
+
+function createHitokoto(hitokoto) {
+	const hitokotoBarItem = vscode.window.createStatusBarItem(2, 5)
+	request('https://v1.hitokoto.cn', function (err, res, body) {
+		if (!err && res.statusCode == 200) {
+			const data = JSON.parse(body)
+			hitokoto['id'] = data.id
+			hitokotoBarItem.color = '#fff'
+			hitokotoBarItem.text = `$(comment) ${data.hitokoto} ----- ${data.from}`
+			hitokotoBarItem.command = `extension.openHitokoto`
+			hitokotoBarItem.tooltip = "去hitokoto查看此条目";
+			hitokotoBarItem.show()
+			setTimeout(() => {
+				hitokotoBarItem.dispose()
+			}, 15000);
+			// vscode.window.setStatusBarMessage(`${data.hitokoto} ----- ${data.from}`,10000);
+		} else {
+			vscode.window.showInformationMessage('API服务器连接失败')
+		}
+	})
+
+}
+
+
+
 exports.activate = activate;
+exports.createHitokoto = createHitokoto;
 
 // this method is called when your extension is deactivated
 function deactivate() {}
-
 module.exports = {
 	activate,
 	deactivate
